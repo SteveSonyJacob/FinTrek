6f436285c1f135d0bd64d1dc41bcbddfa20d1eff
Razin <razinm270@gmail.com>
2025-09-03 18:26:46 +0530

    supabase client and maion tweeks

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { SessionContextProvider } from "@supabase/auth-helpers-react"
+import { supabase } from "./integrations/supabase/client"
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>
+)
